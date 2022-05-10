1f7925f261c2d9dc3b69c496c9a1c84299e410c3
Kalyan Karamsetty <kalyan.karamsetty@gmail.com>
2022-05-10 04:03:19 -0700

    Fixed Status Bar Colors

--- dining_application/App.js
+++ dining_application/App.js
@@ -99,7 +99,7 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { Button, View, Text, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import { Button, View, Text, SafeAreaView, ScrollView, StyleSheet, StatusBar} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
@@ -127,6 +127,9 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
+      <StatusBar
+        barStyle="dark-content"
+      />
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Details" component={DetailsScreen} />
