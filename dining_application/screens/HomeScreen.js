e885f110ea31e34f1153d1a40e663664c6a2687c
Kalyan Karamsetty <kalyan.karamsetty@gmail.com>
2022-05-12 01:15:08 -0700

    UI for when dining halls are closed added

--- dining_application/screens/HomeScreen.js
+++ dining_application/screens/HomeScreen.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { View, Text, SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, Dimensions, Image} from 'react-native';
-import Menubutton from '../components/menubutton';
 import Block from '../components/block';
 import { BlurView } from 'expo-blur';
 import Gradient from 'dining_application/assets/gradient.js'
@@ -11,121 +10,11 @@ import SimpleButton from '../components/simpleButton';
 import BellNotification from '../assets/icons/bell_notification';
 import RedHeart from '../assets/icons/redHeart';
 import MealPlan from '../components/mealPlan';
+import ActiveDiningHalls from '../components/activeDiningHalls';
 import { useFonts } from 'expo-font';
 import FoodTrucks from '../components/foodTrucks';
 
 let hours = new Date().getHours(); 
-let minutes = new Date().getMinutes();
-
-const DATA = [
-    {
-        id: 1,
-        name: "Epicuria", 
-        waitTime: 4, 
-        imageUri: require('dining_application/assets/diningHallImages/epicimage.jpeg'), 
-    },
-    {
-        id: 2,
-        name: "Bruin Plate", 
-        waitTime: 6, 
-        imageUri: require('dining_application/assets/diningHallImages/bplateimage.jpg'), 
-    },
-    {
-        id: 3,
-        name: "De Neve", 
-        waitTime: 6, 
-        imageUri: require('dining_application/assets/diningHallImages/deneve.jpg'), 
-    },
-    {
-        id: 4,
-        name: "Rendezvous", 
-        waitTime: 18, 
-        imageUri: require('dining_application/assets/diningHallImages/rende.jpg'), 
-    },
-    {
-        id: 5,
-        name: "Bruin Cafe", 
-        waitTime: 22, 
-        imageUri: require('dining_application/assets/diningHallImages/bcafe.jpg'), 
-    },
-    {
-        id: 6,
-        name: "The Feast", 
-        waitTime: 34, 
-        imageUri: require('dining_application/assets/diningHallImages/feast.jpg'), 
-    },
-    {
-        id: 7,
-        name: "Bruin Bowl", 
-        waitTime: 21, 
-        imageUri: require('dining_application/assets/diningHallImages/bowl.jpg'), 
-    },
-    {
-        id: 8,
-        name: "The Study", 
-        waitTime: 45, 
-        imageUri: require('dining_application/assets/diningHallImages/study.jpg'), 
-    },
-];
-
-function ActiveDiningHalls(props) {
-    const [loaded] = useFonts({
-        'sf-pro-sb': require('dining_application/assets/fonts/SF-Pro-Text-Semibold.otf'),
-    });
-    
-    if (!loaded) {
-        return null;
-    }
-
-    let mealPeriod = "Dining Halls are currently closed";
-    let closed = true; 
-    if (hours > 6 && hours < 11) {
-        mealPeriod = "Dining Halls open for breakfast"; 
-        closed = false; 
-    } else if (hours > 10 && hours < 16) {
-        mealPeriod = "Dining Halls open for lunch"; 
-        closed = false; 
-    } else if (hours > 20 && minutes > 0) {
-        mealPeriod = "Dining Halls open for late night"; 
-        closed = false; 
-    } else if (hours > 16 && hours < 22) {
-        mealPeriod = "Dining Halls open for dinner"; 
-        closed = false; 
-    } 
-    //closed = true; 
-
-    if(closed == true) {
-        return (
-            <View style={{marginTop: -20}}>
-                <Block>
-                    <Image style={{width: 100, height: 100, alignSelf: "center"}} source={require("dining_application/assets/animojis/sadbear.png")}/>
-                    <Text style={{fontFamily: "sf-pro-sb", fontSize: 14, textAlign: "center", paddingHorizontal: 20, marginBottom: 20}}>Dining halls are closed right now. Dining halls will re-open in 6 hours</Text>
-                    <SimpleButton style={{alignSelf: "center", marginBottom: 10,}} background="true" text="See all dining halls"/>
-                </Block>
-            </View>
-        );
-    }
-    let activeDiningHalls = []; 
-    let sortedData = DATA.slice(); 
-    sortedData.sort(function (a, b) {
-        return a.waitTime - b.waitTime;
-    }); 
-    for (let i = 0; i < sortedData.length; i++) {
-        activeDiningHalls.push(<Menubutton name={sortedData[i].name} waitTime={sortedData[i].waitTime} imageUri={sortedData[i].imageUri} key={sortedData[i].id.toString()}/>); 
-    }
-
-    return (
-        <View>
-            <Text style={{fontFamily: "sf-pro-sb", fontSize: 18}}>{mealPeriod}</Text>
-            <View style={styles.grid}>
-                {activeDiningHalls}
-            </View>
-            <View style={{flexDirection: "row", justifyContent: "flex-end", paddingTop: 5}}>
-                    <SimpleButton text="See all dining halls"/>
-            </View>
-        </View>
-    ); 
-}
 
 function HomeScreenContent({navigation}) {
     const [loaded] = useFonts({
@@ -233,14 +122,6 @@ const styles = StyleSheet.create({
     list: {
         overflow: "visible"
     },
-    grid: { 
-        flex: 1, 
-        flexDirection: 'row', 
-        flexWrap: 'wrap',
-        marginRight: -20,
-        marginTop: 10, 
-    },
-
 });
 
 export default HomeScreen
