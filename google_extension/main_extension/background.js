d02f350f19b35879b451ceb2984c9114f1115aac
kangrui-chen <57692129+chenkangrui@users.noreply.github.com>
2022-05-20 18:55:45 -0700

    Added React-based google extension with pop-up window

--- google_extension/main_extension/background.js
+++
@@ -1,8 +0,0 @@
-
-chrome.action.onClicked.addListener(tab => {
-    let msg = {
-        txt: "hello"
-    }
-    console.log("background script");
-    //chrome.tabs.sendMessage(tab.id, "hello");
-});
