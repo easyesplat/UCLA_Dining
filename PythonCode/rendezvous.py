01174360692039afaad814d59302a009cb7a9b5b
easyesplat <ericchoirr38@gmail.com>
2022-05-23 09:42:11 -0700

    Added Rendezvous webscrape script and added Spice Kitchen script's fixes

--- PythonCode/rendezvous.py
+++ PythonCode/rendezvous.py
@@ -15,17 +15,26 @@ firebase_admin.initialize_app(cred, {
 db = firestore.client()
 db.collection('menu').document('Bruin Plate').delete()
 
-html_link = 'https://menu.dining.ucla.edu/Menus/FeastAtRieber/Today'
+html_link = 'http://menu.dining.ucla.edu/Menus/Rendezvous/today'
 #grabbing the page
 uClient = uReq(html_link)
 page_html = uClient.read()
 uClient.close()
 
+time_periods = ['lunch', 'dinner']
+per_count = 0
+
+#parsing page
 page = soup(page_html, 'html.parser')
 foodlist = page.find_all("li", class_="sect-item")
 
 for food_block in foodlist:
-    col = db.collection(u'menu').document(u'Spice Kitchen at Feast').collection('dinner').document('Spice Kitchen')
+    topic_list = food_block.text.splitlines()
+    topic_list = [x for x in topic_list if x.strip()]
+    main_title = topic_list[0].strip()
+    col = db.collection(u'menu').document(u'Rendezvous').collection(time_periods[per_count]).document(main_title)
+    if main_title == 'Boba':
+        per_count = per_count+1
     dict = {}
     menu_items = food_block.find_all("li", class_="menu-item")
     for item in menu_items:
