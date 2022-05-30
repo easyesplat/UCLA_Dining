471651fa01ea50323f4e580008d4344a055d0e04
kangrui-chen <57692129+chenkangrui@users.noreply.github.com>
2022-05-30 10:53:40 -0700

    Changed main rendering function

--- google_extension/dining_extension/src/App.js
+++ google_extension/dining_extension/src/App.js
@@ -4,68 +4,58 @@ import "./css/App.css";
 import "./css/body_style.css";
 import "./css/header_style.css";
 
-import {ButtonGroup, RenderData} from "./renderFuncs/renderData";
+import CardContainer from "./MainMenu/flipcards.js";
+import {RenderData} from "./renderFuncs/renderTimeData";
 import {UserInput} from "./renderFuncs/userInput";
-
-
-// ----------------- Functions for main page rendering --------------------
-function MainMenu() {
-  return (
-    <div className="App">
-      <div className="container">
-      <div className="row">
-        <img src={require("./assets/diningHallImages/bcafe.jpg")} />
-        <button className="btn">Bruin Café</button>
-      </div>
-      <div className="container">
-        <img src={require("./assets/diningHallImages/deneve.jpg")} />
-        <button className="btn">DeNeve</button>
-      </div>
-      </div>
-
-      <div className="row">
-      <div className="container">
-        <img src={require("./assets/diningHallImages/bplateimage.jpg")} />
-        <button className="btn">Bruin Plate</button>
-      </div>
-      <div className="container">
-        <img src={require("./assets/diningHallImages/epicimage.jpeg")} />
-        <button className="btn">Epicuria</button>
-      </div>
-      </div>
-    </div>
-  )
-}
-
+import Greeting from './renderFuncs/userGreeting.js';
 
 // ------------------ Main rendering class component ------------------
 function App() {  
-  // Hook States
 
   return (
-      <div className="App">
+      <div className="body">
         <div className="topbar">
             <h1> Bear Food Extension </h1>
+            <h2> <Greeting /> </h2>
             <div className="navbar">
-                <a href="index.html"> Home </a>
-                <a href="https://menu.dining.ucla.edu/hours/">Hours</a>
+                <a href="index.html"
+                   className="App-link"
+                   target="_blank"
+                   rel="noopener noreferrer"> Home </a>
+                <a href="https://menu.dining.ucla.edu/hours/"
+                   className="App-link"
+                   target="_blank"
+                   rel="noopener noreferrer">Hours</a>
             <div className="dropdown">
                 <button className="dropbtn">Dining Hall Links &#x25BE;
                     <i className="fa fa-caret-down"></i>
                 </button>
                 <div className="dropdown-content">
-                    <a href="http://menu.dining.ucla.edu/Menus/BruinPlate/Today" onClick={function() { console.log('click'); }}>BruinPlate</a>
-                    <a href="https://menu.dining.ucla.edu/Menus/DeNeve/Today">De Neve</a>
-                    <a href="https://menu.dining.ucla.edu/Menus/Epicuria/Today">Epicuria</a>
+                    <a href="http://menu.dining.ucla.edu/Menus/BruinPlate/Today" 
+                       className="App-link"
+                       target="_blank"
+                       rel="noopener noreferrer">BruinPlate</a>
+                    <a href="https://menu.dining.ucla.edu/Menus/DeNeve/Today"
+                       className="App-link"
+                       target="_blank"
+                       rel="noopener noreferrer"> De Neve </a>
+                    <a href="https://menu.dining.ucla.edu/Menus/Epicuria/Today"
+                       className="App-link"
+                       target="_blank"
+                       rel="noopener noreferrer">Epicuria</a>
                 </div>
             </div>
             </div>
-        
-        <ButtonGroup buttons={["BPlate", "De Neve", "Epicuria"]} />
-        <div> <RenderData /> </div>
-        <MainMenu />
         </div>
-        
+        <p> <br></br> <br></br> </p>
+        <div style={bodyStyles}>
+          <p> Click 'Home' to refresh page & reload data</p>
+          <p> Click on the first box to display realtime capacity data:</p>
+          <p> We could show wait times on the back of the flip cards </p>
+          <RenderData /> 
+          <p> <br></br> </p>
+        </div>
+        <CardContainer />
       </div>
   )
 }
